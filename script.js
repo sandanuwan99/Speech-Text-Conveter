044e758b882ade972bef65120df357109e9872c2
janitha sandanuwan <janithasandanuwa@gmail.com>
2025-06-08 09:39:40 +0530

    update all files

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-const resultEi = document.getElementById('result');
+const resultElement = document.getElementById("result");
 let recognition;
-''
 
-function startConverting(){
-     if('webkitSpeechRecognition' in window)
+function startConverting() {
+    if ('webkitSpeechRecognition' in window) {
         recognition = new webkitSpeechRecognition();
-        setUpRecognition(recognition);
+        setupRecognition(recognition);
         recognition.start();
+    } else {
+        alert("Speech Recognition not supported in this browser. Please use Google Chrome.");
+    }
 }
 
-function setUpRecognition(recognition){
+function setupRecognition(recognition) {
     recognition.continuous = true;
     recognition.interimResults = true;
     recognition.lang = 'en-US';
+
+    recognition.onresult = function (event) {
+        const { finalTranscript, interTranscript } = processResult(event.results);
+        resultElement.innerHTML = finalTranscript + '<span style="color:gray;">' + interTranscript + '</span>';
+    };
+
+    recognition.onerror = function (event) {
+        console.error("Speech recognition error:", event.error);
+    };
 }
 
-function processResult(result){
+function processResult(results) {
+    let finalTranscript = '';
+    let interTranscript = '';
 
+    for (let i = 0; i < results.length; i++) {
+        let transcript = results[i][0].transcript;
+        transcript = transcript.replace(/\n/g, "<br>");
 
+        if (results[i].isFinal) {
+            finalTranscript += transcript + " ";
+        } else {
+            interTranscript += transcript;
+        }
+    }
+
+    return { finalTranscript, interTranscript };
 }
 
-
-
-
-
-
-
-
-
-
-
-function stopConverting(){
-
+function stopConverting() {
+    if (recognition) {
+        recognition.stop();
+    }
 }
